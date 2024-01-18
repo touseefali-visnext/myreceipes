3747651607dcd4e36fa19676d069c10ad0dcfebd
touseefali-visnext <touseef.ali@visnext.com>
2024-01-18 19:42:14 +0500

    Implement edit and delete

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -22,6 +22,27 @@ class RecipesController < ApplicationController
         end
     end
 
+    def edit
+        @recipe = Recipe.find(params[:id])
+    end
+
+    def update
+        @recipe = Recipe.find(params[:id])
+        if @recipe.update(recipe_params)
+            flash[:success] = "Recipe was updated successfully"
+            redirect_to(recipe_path(@recipe))
+        else 
+            render "edit"
+        end
+    end
+
+    def destroy
+        @recipe = Recipe.find(params[:id])
+        @recipe.destroy
+        flash[:success] = "Recipe deleted successfully"
+        redirect_to recipes_path, status: :see_other
+    end
+
     private
         def recipe_params
             params.require(:recipe).permit(:name, :description)
