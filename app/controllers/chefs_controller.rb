fc2d75a35a4fa19eb1f21323e82ab9e91a133362
touseefali-visnext <touseef.ali@visnext.com>
2024-01-19 17:21:08 +0500

    Add edit action with tests

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -18,6 +18,21 @@ class ChefsController < ApplicationController
         @chef = Chef.find(params[:id])
     end
 
+    def edit
+        @chef = Chef.find(params[:id])
+    end
+
+    def update
+        @chef = Chef.find(params[:id])
+
+        if @chef.update(chef_params)
+            flash[:success] = "Your account was updated successfully"
+            redirect_to @chef
+        else
+            redner "edit"
+        end
+    end
+
     private
         def chef_params 
             params.require(:chef).permit(:chefname, :email, :password, :password_confirmation)
---
+++ test/integration/chefs_edit_test.rb
@@ -0,0 +1,28 @@
+require "test_helper"
+
+class ChefsEditTest < ActionDispatch::IntegrationTest
+  def setup
+    @chef = Chef.create!(chefname: "Touseef", email: "touseef@example.com", password: "password", password_confirmation: "password")
+    # @recipe = Recipe.create(name: "Vegetale saute", description: "Greate vegetable saute, add vegetable and oil", chef: @user)
+  end
+
+  test "reject an invalid edit signup" do
+    get edit_chef_path(@chef)
+    assert_template "chef/edit"
+    patch chef_path(@chef), params: { chef: { chefname: " ", email: "touseef@example.com" } }
+    assert_template "chefs/edit"
+    assert_select "h2.card-title"
+    assert_select "div.card-body"
+  end
+
+  test "accept valid signup" do
+    get edit_chef_path(@chef)
+    assert_template "chef/edit"
+    patch chef_path(@chef), params: { chef: { chefname: "Touseef Ali", email: "touseef1@example.com" } }
+    assert_redirected_to @chef
+    assert_not flash.empty?
+    @chef.reload
+    assert_match "Touseef Ali", @chef.chefname
+    assert_match "touseef1@example.com", @chef.email
+  end
+end
